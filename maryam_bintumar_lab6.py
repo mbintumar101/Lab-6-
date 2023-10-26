09bf33fdbdca33fdbffb0916ef357ee37868b5bc
maryambintumar <mbintumar@ufl.ed>
2023-10-25 22:21:01 -0400

    Commented Decode Definition

--- maryam_bintumar_lab6.py
+++ maryam_bintumar_lab6.py
@@ -5,12 +5,12 @@ def encode_password(password):
         encoded_password += encoded_digit
     return encoded_password
 
-def decode_password(encoded_password):
-    decoded_password = ""
-    for digit in encoded_password:
-        decoded_digit = str((int(digit) - 3) % 10)
-        decoded_password += decoded_digit
-    return decoded_password
+#def decode_password(encoded_password):
+    #decoded_password = ""
+    #for digit in encoded_password:
+        #decoded_digit = str((int(digit) - 3) % 10)
+        #decoded_password += decoded_digit
+    #return decoded_password
 
 while True:
     print("Menu")
