951236b7c60efd679c7629820515d749007c2de7
maryambintumar <mbintumar@ufl.ed>
2023-10-25 22:18:23 -0400

    Inserting Completed Project

---
+++ maryam_bintumar_lab6.py
@@ -0,0 +1,37 @@
+def encode_password(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+def decode_password(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    option = input("Please enter an option: ")
+
+    if option == "1":
+        password = input("Please enter your password to encode: ")
+        encoded_password = encode_password(password)
+        print("Your password has been encoded and stored!")
+
+    elif option == "2":
+        if 'encoded_password' in locals():
+            decoded_password = decode_password(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+        else:
+            print("No password has been encoded yet. Please encode a password first.")
+
+    elif option == "3":
+        break
